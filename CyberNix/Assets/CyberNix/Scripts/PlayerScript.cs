ffa46983c6e7870fbf03f7dfd22c755835d89a3d
Nougato <moitheos@gmail.com>
2021-05-01 15:36:22 +0200

    add blinking and Invulnerability

--- CyberNix/Assets/CyberNix/Scripts/PlayerScript.cs
+++ CyberNix/Assets/CyberNix/Scripts/PlayerScript.cs
@@ -5,15 +5,49 @@ using UnityEngine;
 public class PlayerScript : MonoBehaviour
 {
     public Rigidbody Rigidbody;
+    public MeshRenderer[] BodyParts;
+    public float BlinkSpeed = .05f;
+    public float InvicibleTime = 2f;
+
+    private bool CanDie;
+
     // Start is called before the first frame update
     void Start()
     {
+        CanDie = true;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (CanDie)
+        {
+            CanDie = false;
+            StartCoroutine(Invulnerability());
+        }
+    }
+
+    private IEnumerator Invulnerability()
+    {
+        var endTime = Time.time + InvicibleTime;
+        while(Time.time < endTime)
+        {
+            foreach(MeshRenderer bodyPart in BodyParts)
+            {
+                bodyPart.enabled = !bodyPart.enabled;
+                yield return new WaitForSeconds(BlinkSpeed);
+            }
+        }
+        foreach (MeshRenderer bodyPart in BodyParts)
+        {
+            bodyPart.enabled = true;
+        }
+        CanDie = true;
     }
 
     public void Jump(float JumpHeight)
