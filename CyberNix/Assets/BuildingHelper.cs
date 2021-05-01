4f6630a8ed75e7559c3e6d56a99533ae43638efd
seb776 <sebastien.maire@epitech.eu>
2021-05-01 16:09:04 +0200

    Added diversity to buildings

--- CyberNix/Assets/BuildingHelper.cs
+++ CyberNix/Assets/BuildingHelper.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class BuildingHelper : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public List<Texture> BuildingTextures;
+    public List<Texture> JapTextures;
+    public List<Color> Colors;
 
-    // Update is called once per frame
-    void Update()
+    public MeshRenderer JapQuad;
+    public MeshRenderer BuildingQuad;
+    public Light Light;
+
+    private void Start()
     {
-        
+        JapQuad.material.SetColor("_Color", Colors[Random.Range(0, Colors.Count)]*2.0f);
+        Light.color = Colors[Random.Range(0, Colors.Count)];
+
+        if (BuildingTextures != null && BuildingTextures.Count > 0)
+            BuildingQuad.material.SetTexture("_MainTex", BuildingTextures[Random.Range(0, BuildingTextures.Count)]);
+        if (JapTextures != null && JapTextures.Count > 0)
+            JapQuad.material.SetTexture("_MainTex", JapTextures[Random.Range(0, JapTextures.Count)]);
     }
 }
