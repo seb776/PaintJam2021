b5a722ca5ebea6f3960e878838d2d7795ad64a48
seb776 <sebastien.maire@epitech.eu>
2021-05-02 20:34:05 +0200

    Added code to show gained points

---
+++ CyberNix/Assets/GainPoint.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GainPoint : MonoBehaviour
+{
+    private static float _visibleDuration = 1.0f;
+    private static float _disappearDuration = 1.0f;
+    public float SpeedUp;
+    public List<Color> Colors;
+    public TextMesh Text;
+
+    void Start()
+    {
+        Gain(50);
+    }
+
+    public void Gain(int point)
+    {
+        Text.color = Colors[Random.Range(0, Colors.Count)];
+        Text.text = $"+{point}";
+        StartCoroutine(_handleGainPoint());        
+    }
+
+    IEnumerator _handleGainPoint()
+    {
+        yield return new WaitForSeconds(_visibleDuration);
+
+        float step = 1.0f / 5.0f;
+        float time = _disappearDuration;
+        while (time > 0.0f)
+        {
+            float coef = (time / _disappearDuration);
+            Text.color = new Color(Text.color.r, Text.color.g, Text.color.b, coef);
+            time -= step;
+            yield return new WaitForSeconds(step);
+        }
+        Text.color = new Color(Text.color.r, Text.color.g, Text.color.b, 0.0f);
+        Destroy(this.gameObject);
+    }
+    public void Update()
+    {
+        this.transform.position += Vector3.up * SpeedUp*Time.deltaTime;
+    }
+
+}
