edd6f11e703fbc835b951414ea3739ab9e7197e3
seb776 <sebastien.maire@epitech.eu>
2021-05-02 14:32:56 +0200

    Added main menu

---
+++ CyberNix/Assets/HandleMenu.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class HandleMenu : MonoBehaviour
+{
+    public string StartSceneName;
+    public GameObject MainMenuHolder;
+    public GameObject CreditsHolder;
+    public GameObject HighScoresHolder;
+
+    public GameObject HighScoresPrefab;
+
+    public void PressStart()
+    {
+        MainMenuHolder.SetActive(false);
+        SceneManager.LoadSceneAsync(StartSceneName, LoadSceneMode.Single);
+    }
+
+    public void PressCredits()
+    {
+        MainMenuHolder.SetActive(false);
+        CreditsHolder.SetActive(true);
+    }
+    public void PressPreviousToMenu()
+    {
+        MainMenuHolder.SetActive(true);
+        HighScoresHolder.SetActive(false);
+        CreditsHolder.SetActive(false);
+    }
+    public void PressScores()
+    {
+        MainMenuHolder.SetActive(false);
+        HighScoresHolder.SetActive(true);
+    }
+}
